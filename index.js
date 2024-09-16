870c5177d51941cc768a9baabe4ec2019ff73159
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-09-16 09:57:43 -0300

    updates

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
+let prods = [];
+
 function buscarProdutos(){
     fetch('https://fakestoreapi.com/products')
     .then(resposta => resposta.json())
     .then(listaProdutos => {
+        prods = listaProdutos;
+        carregarProdutos(prods);
+    })
+}
 
-        listaProdutos.map(produto => {
-            produtos.innerHTML += `
-                <li class="">
-                    <div>
-                        <img src="${produto.image}" alt="" class="w-full">
-                        <span>${produto.rating.rate}</span>
-                    </div>
-                    <div>
-                        <h3>${produto.title}</h3>
-                        <h6>${produto.category}</h6>
-                        <h2>R$ ${produto.price}</h2>
-                    </div>
-                </li>
-            `;
+function buscarCategorias(){
+    fetch("https://fakestoreapi.com/products/categories")
+    .then(response => response.json())
+    .then((listaCategorias) => {
+        listaCategorias.map(categoria => {
+            categorias.innerHTML += `<option>${categoria}</option>`;
         })
     })
 }
 
-buscarProdutos()
+function carregarProdutos(listaProdutos){
+    produtos.innerHTML = ``;
+    listaProdutos.map(produto => {
+        produtos.innerHTML += `
+            <li class="bg-white p-3 rounded-lg">
+                <div class="relative mb-5">
+                    <img src="${produto.image}" alt="" class="w-full h-[250px] object-contain">
+                    <span class="absolute top-[16px] right-[16px] bg-gray-600 text-white font-bold text-xs px-2 py-1 rounded-sm">${produto.rating.rate}</span>
+                </div>
+                <div class="block">
+                    <h6 class="bg-gray-300 px-2 inline-block font-bold rounded-sm mb-1">${produto.category}</h6>
+                    <h3 class="text-lg truncate" title="${produto.title}">${produto.title}</h3>
+                    <h2 class="text-[32px]">R$ ${produto.price.toFixed(2)}</h2>
+                </div>
+            </li>
+        `;
+    })
+}
+
+function ordenarProdutos(){
+    if(ordenar.value == 1){
+        prods.sort((a ,b) => b.price - a.price);
+        carregarProdutos(prods);
+    }
+    if(ordenar.value == 2){
+        prods.sort((a ,b) => b.rating.rate - a.rating.rate);
+        carregarProdutos(prods);
+    }
+}
+
+function mostrarUmaCategoria(){
+    let lis = document.querySelectorAll('li');
+    lis.forEach(li => {
+        if(li.children[1].children[0].innerText != categorias.value){
+            li.classList.add('hidden')
+        }else{
+            li.classList.remove('hidden')
+        }
+    })
+}
+
+buscarProdutos();
+buscarCategorias();
